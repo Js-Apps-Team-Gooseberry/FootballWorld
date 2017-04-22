d181815f537ab81526790bc9d1036e924c5ce420
dimitar-pechev <dimpechev@gmail.com>
2017-04-22 20:38:55 +0300

    sample bootstrap theme added

--- server/config/express.js
+++ server/config/express.js
@@ -2,12 +2,14 @@
 
 const express = require('express'),
     cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    morgan = require('morgan');
 
 let app = express();
 app.use('/public', express.static('client'));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 module.exports = app;
