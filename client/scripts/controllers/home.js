6526a82051ece1d326ec07df06936e71f18dea86
dimitar-pechev <dimpechev@gmail.com>
2017-05-07 12:40:30 +0300

    home page responsive now

--- client/scripts/controllers/home.js
+++ client/scripts/controllers/home.js
@@ -1,14 +1,25 @@
 import { compile } from 'templates-compiler';
 import $ from 'jquery';
 import * as statsService from 'stats-service';
+import { getNotDeletedArticlesByPage as getLatestNews } from 'news-service';
+import { getNotDeletedArticlesByPage as getLatestArticles } from 'articles-service';
 
 const $mainContainer = $('#main-container');
 
 function get() {
-    statsService.getStandings()
-        .then(standings => {
-            return compile('home', standings);
-        })    
+    let page = 1;
+    let pageSize = 3;
+
+    Promise.all([statsService.getStandings(), getLatestNews(page, pageSize), getLatestArticles(page, pageSize)])
+        .then(([standings, news, articles]) => {
+            let data = {
+                standings,
+                news: news.newsEntries,
+                articles: articles.articles
+            };
+            
+            return compile('home', data);
+        })
         .then(html => $mainContainer.html(html));
 }
 
